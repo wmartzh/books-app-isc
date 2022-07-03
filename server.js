870e3b0f55ccd592a37eb2c9cfa96a121669bd0a
wmartzh <wmartzh@gmail.com>
2022-07-02 19:40:00 -0600

    Add helmet

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const router = require('./router')
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const router = require("./router");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 // app.options('*',cors())
-app.use('/api',router);
+app.use(helmet());
+app.use("/api", router);
 
-app.get('/',(req,res)=>{
-  res.send("Hello")
-})
-app.listen(process.env.PORT, process.env.HOST, ()=>{
-  console.log(`⚡Server listening at http://${process.env.HOST}:${process.env.PORT}`)
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `⚡Server listening at http://${process.env.HOST}:${process.env.PORT}`
+  );
 });
